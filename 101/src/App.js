a5f5aa5217f6cc0bf0e0774c040bef8e170671b7
Treek <nbatre1@yahoo.com>
2019-12-25 10:38:00 -0600

    keep moving forward

--- 101/src/App.js
+++ 101/src/App.js
@@ -4,12 +4,21 @@ import{Link,BrowserRouter as Router,Route ,Switch} from 'react-router-dom';
 import Home from './Components/Home';
 import About from './Components/About';
 import Navbar from './Components/Navbar/Navbar';
+import Signup from './Components/Signup';
+import Login from './Components/Login';
+import Help from './Components/Help';
 
 
 function App() {
   return (
     <Router>
       <Navbar/>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About}/>
+      <Route exact path="/help" component={Help}/>
+      <Route exact path="/loginin" component={Login}/>
+      <Route exact path="/signup" component={Signup}/>
+
     </Router>
   );
 }
---
+++ 101/src/Components/Help.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+
+
+
+class Help extends Component{
+
+
+
+
+
+
+    render(){
+
+
+
+      return(
+          <div>
+            <h1>Help</h1>
+          </div>
+      )
+    }
+}
+
+
+export default Help
